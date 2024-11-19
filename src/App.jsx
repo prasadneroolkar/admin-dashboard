20dbcca6e2a3f3be983e757412adcf4e3940e9fe
prasad <prasadneroolkar90@gmail.com>
2024-11-19 12:04:37 +0530

    dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
 import "./styles/app.scss";
+
+const Dashboard = lazy(() => import("./pages/Dashboard")); //code splitting
 function App() {
-  return <>app</>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/admin/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
